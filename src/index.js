d0485df3a743918f685b2c056e4b3a16fb644d2a
April Monoceros <git@aprilmonoceros.com>
2019-01-30 00:31:14 -0800

    *Adding additional pictures *Some additional CSS classes (for the title, and differentiating between right and left of the picture) *Changed default pic *Removed commented-out code from an earlier, unsuccessful attempt *Renamed some of the project titles

--- src/index.js
+++ src/index.js
@@ -1,118 +1,14 @@
-/*
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-const pics = {
-    loveStory: require('./images/LoveStoryScreenshot.png'),
-    diploma: require('./images/AprilMastersDiploma.jpeg')
-}
-
-var total = 0;
-
-function SetPicture(props) {
-
-    // if(props.picture === 'diploma') {
-    //
-    // }
-    if (props.picture === 'loveStory') {
-        return pics.loveStory
-        // return <img alt={"Love Story"} src={pics.loveStory} />;
-    }
-    console.log("Code was here");
-    return (
-        /!*<img alt={"Diploma"} src={pics.diploma} />*!/
-        pics.diploma
-    )
-}
-
-class Portfolio extends React.Component {
-    constructor(props) {
-        super(props);
-        this.handlePictureChange = this.handlePictureChange.bind(this);
-        this.state = {picture: 'default'};
-    }
-
-    handlePictureChange(e) {
-        if(this.state.toString() !== e.toString() )
-        {
-            console.log("Picture Entered");
-            if(total < 5) {
-                this.setState({picture: e.toString()});
-                //this.forceUpdate();
-                total++;
-            }
-            //done = true;
-        } else {
-            console.log("Picture Not entered");
-        }
-    }
-
-    render() {
-        console.log("Code was rendering");
-        // function makeLoveStory(e) {
-        //     // currImage = pics.loveStory;
-        //     // currImageAlt = "Love Story";
-        //     e.target.setAttribute('src', pics.loveStory);
-        //     e.target.setAttribute('alt', "loveStory");
-        //     // previewImage = (<img alt ={currImageAlt} src={currImage}/>);
-        //     // ReactDOM.render(page, document.getElementById('root'));
-        // }
-
-        //let currImage = pics.diploma;
-        //let currImageAlt = "Diploma";
-
-        let previewImage = (
-            //{SetPicture}
-            <img alt="" src={SetPicture(this.props)}/>
-            // {/!*<img alt = "Love Story Demo preview" src={require('./images/LoveStoryScreenshot.png')}/>*!/}
-            // {/!*require('./images/LoveStoryScreenshot.png')*!/}
-            // {/!*<img src={require('./images/LoveStoryScreenshot.png')} alt="Love Story demo"/>*!/}
-        )
-
-        //let page = (
-        return (
-            <div>
-                {/!*<div onMouseEnter={makeLoveStory}>*!/}
-                <div>
-                    {/!*<img alt={"Diploma"} src={pics.diploma} />*!/}
-                    {previewImage}
-                </div>
-                <div onMouseEnter={this.handlePictureChange("loveStory")}>
-                    <a href="Somewhere">
-                        Love Story
-                    </a><br/>
-                </div>
-                <div>
-                    <a href="Somewhere">Diploma</a><br/>
-                </div>
-            </div>
-        );
-        //return page;
-    }
-}
-
-// ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Portfolio />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
-*/
-
-
 import React, { Component } from 'react';
 import { render } from 'react-dom';
 import './index.css';
 
 const pics = {
-    loveStory: require('./images/LoveStoryScreenshot.png'),
-    diploma: require('./images/AprilMastersDiploma.jpeg'),
-    plantProject: require('./images/PlantProjectVideoThumbnail.png'),
-    deutschTelekom: require('./images/XGM.png')
+    loveStory: require('./images/LoveStoryScreenshot_Square.png'),
+    diploma: require('./images/AprilMastersDiploma_Square.jpeg'),
+    plantProject: require('./images/PlantProjectVideoThumbnail_Square.png'),
+    deutschTelekom: require('./images/XGM_Square.png'),
+    bioPic: require('./images/AprilWithSkeleton_Square.jpg'),
+    reactPortfolio: require('./images/PortfolioV1_Square.png')
 };
 
 class AppTry extends Component {
@@ -122,7 +18,7 @@ class AppTry extends Component {
         this.state = {
             name: 'React',
             picture: "",
-            pic: pics.diploma
+            pic: pics.bioPic
         };
     }
 
@@ -140,38 +36,49 @@ class AppTry extends Component {
         if(pictureName === "deutschTelekom" ) {
             this.setState({pic:pics.deutschTelekom});
         }
-        // if(this.state.picture !== pictureName )
-        // {
-        //     console.log("Picture Entered");
-        //     //this.forceUpdate();
-        // } else {
-        //     console.log("Picture Not entered");
-        // }
-        console.log(this.state.picture + " " + pictureName);
+        if(pictureName === "bioPic") {
+            this.setState( {pic:pics.bioPic});
+        }
+        if(pictureName === "reactPortfolio") {
+            this.setState( {pic:pics.reactPortfolio});
+        }
+        //For Debugging:
+        //console.log(this.state.picture + " " + pictureName);
     }
 
     render() {
         return (
-            //<div onMouseEnter={() => this.handlePictureChange("loveStory")} onMouseLeave={() => this.handlePictureChange("diploma")}>
-            <div className="row">
-                <div className="projects">
-                    <p onMouseEnter={() => this.handlePictureChange("plantProject")}>
-                        Plant Project
-                    </p>
-                    <p onMouseEnter={() => this.handlePictureChange("deutschTelekom")}>
-                        Deutsch Telekom Demonstration Project
-                    </p>
+            <div>
+                <div className="row">
+                    <div className="bio-title">
+                        <p onMouseEnter={() => this.handlePictureChange("bioPic")}>
+                            April Monoceros Bio
+                        </p>
+                    </div>
                 </div>
-                <div className="image-div">
-                    <span><img className="center-img" alt="" src={this.state.pic}/></span>
-                </div>
-                <div className="projects">
-                    <p onMouseEnter={() => this.handlePictureChange("loveStory")}>
-                        Love Story demo
-                    </p>
-                    <p onMouseEnter={() => this.handlePictureChange("diploma")}>
-                        Computer Science Masters
-                    </p>
+                <div className="row">
+                    <div className="projects-left">
+                        <p onMouseEnter={() => this.handlePictureChange("plantProject")}>
+                            Remote Plant Watering
+                        </p>
+                        <p onMouseEnter={() => this.handlePictureChange("deutschTelekom")}>
+                            Smart Glasses Machine Monitor
+                        </p>
+                    </div>
+                    <div className="image-div">
+                        <span><img className="center-img" alt="" src={this.state.pic}/></span>
+                    </div>
+                    <div className="projects-right">
+                        <p onMouseEnter={() => this.handlePictureChange("loveStory")}>
+                            Love Story Real-Time Demo
+                        </p>
+                        <p onMouseEnter={() => this.handlePictureChange("diploma")}>
+                            Computer Science Masters
+                        </p>
+                        <p onMouseEnter={() => this.handlePictureChange("reactPortfolio")}>
+                            React Portfolio Site
+                        </p>
+                    </div>
                 </div>
             </div>
         );
