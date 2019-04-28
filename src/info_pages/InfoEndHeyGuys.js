2a9c62b280698f509385066ee311537f0d574d27
April Monoceros <10776123+AdeptApril@users.noreply.github.com>
2019-04-27 23:30:44 -0700

    * Expanded info on the End Hey Guys project

--- src/info_pages/InfoEndHeyGuys.js
+++ src/info_pages/InfoEndHeyGuys.js
@@ -8,10 +8,12 @@ export default class InfoEndHeyGuys extends React.Component{
             <div className="info-div">
                 <p className="paragraph-header">Intro</p>
                 <p>April went to a hackathon, learned enough Javascript to be useful, and helped make a fun project.</p>
-                <p></p>
-                <p></p>
-                <p className="paragraph-header"></p>
-                <p></p>
+                <p className="paragraph-header">Further Project Details</p>
+                <p>The preview image shows one of the middle versions of the project, where the program would slowly display text as if it were being typed in or recorded and transcribed, and then displayed possibly problematic phrases and possible alternative ways of saying things.</p>
+                <p>This was not meant as a political statement -- just that oftentimes we're not entirely aware of our words and actively _want_ to change how we say things in order to keep everyone involved and valued.</p>
+                <p>April's team envisioned future versions of the project where speech recognition could be used, and then a chime played when a variety of phrases were used. We doubted it'd be something to use all the time, but could be useful as a thought-provoking experiment.</p>
+                <p className="paragraph-header">April's contribution</p>
+                <p>April joined the team, helped flesh out the idea, and given her graduate-school background, offered to code the lookup table portion of the project. She accomplished this goal, and the team wound up with a solid presentation of the product and idea.</p>
             </div>
         )
     }
