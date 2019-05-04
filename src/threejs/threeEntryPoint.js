fd84e5fcd92395e7683df5cc76da88f3e5d9da76
April Monoceros <10776123+AdeptApril@users.noreply.github.com>
2019-05-03 23:03:50 -0700

    * Added MIT license, as specified in the place where I got the base code from.

--- src/threejs/threeEntryPoint.js
+++ src/threejs/threeEntryPoint.js
@@ -1,3 +1,36 @@
+/*
+https://itnext.io/how-to-use-plain-three-js-in-your-react-apps-417a79d926e0?gi=7b43422e867b
+...was my starting point for how to get three.js working, and while my code is heavily modified in many areas, it
+certainly was heavily influenced by the code at:
+https://github.com/PierfrancescoSoffritti/pierfrancescosoffritti.com/tree/master/src/components/home/header/threejs
+
+...and that code comes with the MIT License. Since this file is the one closest to what's in his repository, this
+is where I'm placing it. If someone comes along here, though, I'm happy to release my derivative code under the
+same license, with my changes being copyright April Monoceros.
+
+MIT License
+
+Copyright (c) 2019 Pierfrancesco Soffritti
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
 import SceneManager from './SceneManager';
 
 export default container => {
