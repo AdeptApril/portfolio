00c6a702b5eeb92d977ef1ddfd95782d5e38b825
April Monoceros <10776123+AdeptApril@users.noreply.github.com>
2019-11-26 23:15:31 -0800

    * Added another paragraph to the Juggling text

--- src/info_pages/InfoJuggling.js
+++ src/info_pages/InfoJuggling.js
@@ -6,9 +6,10 @@ export default class InfoJuggling extends React.Component{
     render() {
         return (
             <div className="info-div">
-                <p className="paragraph-header">Intro</p>
-                <p>April is a skilled juggler, having managed to successfully juggle seven balls (at once, for those literal-minded), and even more skilled as a teacher, having taught hundreds of people how to juggle or use other props.</p>
-                <p>While this is a little further afield than other projects on this site, she has enjoyed showing how she can explain complicated topics in straight-forward ways. Teaching you how to juggle is one of the ways she can demonstrate her skill in communicating, while doing something that looks pretty neat.</p>
+              <p className="paragraph-header">Intro</p>
+              <p>April is a skilled juggler, having managed to successfully juggle seven balls (at once, for those literal-minded), and even more skilled as a teacher, having taught hundreds of people how to juggle or use other props.</p>
+              <p>While this is a little further afield than other projects on this site, she has enjoyed showing how she can explain complicated topics in straight-forward ways. Teaching you how to juggle is one of the ways she can demonstrate her skill in communicating, while doing something that looks pretty neat.</p>
+              <p>The picture is from a post-apocalyptic LARP where a woman juggling fire torches while wearing a corset and leather skirt is entirely on theme. While April is best at balls, she enjoys the fun and challenges of other props too -- like juggling fire torches, which is made more difficult because the bright flames means that a juggler can't really see the handles to catch them, and thus has to predict where it is based on where the flame is.</p>
             </div>
         )
     }
